eb3cbca577acb2ab856636aae80c03a1aa5f6f98
zdearo <adryelzd77@outlook.com>
2025-02-15 12:24:18 -0300

    fix: Update getAuthUrl method to include state parameter

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -16,27 +16,25 @@ class AuthService extends BaseService
         $this->uri = $region->domain();
     }
 
-    public function getAuthUrl(string $redirectUri, string $clientId): string
+    public function getAuthUrl(string $redirectUri, string $clientId, string $state): string
     {
-        return "https://auth.{$this->uri}/authorization?response_type=code&client_id={$clientId}&redirect_uri={$redirectUri}";
+        return "https://auth.{$this->uri}/authorization?response_type=code&client_id={$clientId}&redirect_uri={$redirectUri}&state={$state}";
     }
 
     public function getToken(string $clientId, string $clientSecret, string $code, string $redirectUri)
     {
-        $response = $this->request('POST', 'http://api.mercadolibre.com/oauth/token', [
+        return $this->request('POST', 'http://api.mercadolibre.com/oauth/token', [
             'grant_type'    => 'authorization_code',
             'client_id'     => $clientId,
             'client_secret' => $clientSecret,
             'code'          => $code,
             'redirect_uri'  => $redirectUri,
         ]);
-
-        return $response;
     }
 
     public function refreshToken(string $clientId, string $clientSecret, string $refreshToken)
     {
-        $response = $this->request('POST', 'http://api.mercadolibre.com/oauth/token', [
+        return $this->request('POST', 'http://api.mercadolibre.com/oauth/token', [
             'form_params' => [
                 'grant_type'    => 'refresh_token',
                 'client_id'     => $clientId,
@@ -44,8 +42,6 @@ class AuthService extends BaseService
                 'refresh_token' => $refreshToken,
             ],
         ]);
-
-        return $response;
     }
 }
 
